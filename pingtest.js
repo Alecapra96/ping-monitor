a33f2159a23ec7b68b68655ec1dbcc675ac62c55
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2022-08-22 17:11:32 -0300

    Update with a loader and timestamp

--- pingtest.js
+++ pingtest.js
@@ -4,15 +4,30 @@ const showBanner = require('node-banner');
 
 
 console.clear();
+const P = ['\\', '|', '/', '-'];
+let x = 0;
+const loader = setInterval(() => {
+  process.stdout.write(`\r${P[x++]}`);
+  x %= P.length;
+}, 250);
+
+setTimeout(() => {
+  clearInterval(loader);
+}, 5000);
 (async () => {
     await showBanner('Ping monitor', 'A tool by @alecapra96');
+    console.log("")
+    console.log("Wait 10 second when the loading end to see the pings")
+
+
 })();
+
 var host2 = [
-    { ip: "XXX.XXX.XXX.XXX", desc: 'DESCRIPCION!', status: "ONLINE"},
-    { ip: "XXX.XXX.XXX.XXX", desc: 'DESCRIPCION!', status: "ONLINE"},
-    { ip: "XXX.XXX.XXX.XXX", desc: 'DESCRIPCION!', status: "ONLINE"},
-    { ip: "XXX.XXX.XXX.XXX", desc: 'DESCRIPCION!', status: "ONLINE"},
-    { ip: "XXX.XXX.XXX.XXX", desc: 'DESCRIPCION!', status: "ONLINE"},
+    { ip: "192.168.12.157", Description: 'DESCRIPCION!', status: ('\x1b[32m ONLINE \x1b[0m')},
+    { ip: "XXX.XXX.XXX.XXX", Description: 'DESCRIPCION!', status: ('\x1b[32m ONLINE \x1b[0m')},
+    { ip: "XXX.XXX.XXX.XXX", Description: 'DESCRIPCION!', status: ('\x1b[32m ONLINE \x1b[0m')},
+    { ip: "XXX.XXX.XXX.XXX", Description: 'DESCRIPCION!', status: ('\x1b[32m ONLINE \x1b[0m')},
+    { ip: "XXX.XXX.XXX.XXX", Description: 'DESCRIPCION!', status: ('\x1b[32m ONLINE \x1b[0m')},
   ]
 var frequency = 5000;
 
@@ -30,7 +45,16 @@ ping.sys.probe( host.ip , function(isAlive) {
         count++
         if (count === 3){
             if (countConsoleOutput < 1){
-                host.status = ('\x1b[91m \x1b[37m \x1b[41m OFFLINE \x1b[0m');
+                let date_ob = new Date();
+                let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+                let date = ("0" + date_ob.getDate()).slice(-2);
+
+                let hours = date_ob.getHours();
+                let minutes = date_ob.getMinutes();
+                let dateOFFLINE = ( hours + ":" + minutes + " - " + date+ "/" + month  );
+
+
+                host.status = ('\x1b[91m \x1b[37m \x1b[41m OFFLINE \x1b[0m') + " at "+ dateOFFLINE;
                 countConsoleOutput = 2;
             
             }
@@ -41,6 +65,7 @@ ping.sys.probe( host.ip , function(isAlive) {
     if(isAlive){
 
         if (countConsoleOutput > 1){
+
             host.status = ('\x1b[32m ONLINE \x1b[0m');
             countConsoleOutput = 0;
         }
