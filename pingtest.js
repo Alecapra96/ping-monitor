690e01aea85cf033feb5a7a65d8d11e1f7c0abdc
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2022-08-22 11:39:41 -0300

    Table created with colors

--- pingtest.js
+++ pingtest.js
@@ -1,27 +1,46 @@
 var ping = require('ping');
+var Table = require('easy-table')
 console.clear();
-var host2 = ['192.168.12.105','192.168.12.153','192.168.12.24', '192.168.12.29','192.168.13.118','192.168.13.120','192.168.13.185','192.168.13.155','192.168.13.126','192.168.13.110','192.168.13.178','192.168.13.113','192.168.13.176','192.168.13.100','192.168.12.26','192.168.12.29','192.168.13.121','192.168.12.129'];
+var host2 = [
+    { ip: "192.168.12.24", desc: 'Remota FSU', status: "ONLINE"},
+    { ip: "192.168.12.29", desc: 'Remota FSU', status: "ONLINE"},
+    { ip: "192.168.13.118", desc: 'Certificados', status: "ONLINE"},
+    { ip: "192.168.13.120", desc: 'Certificados', tatus: "ONLINE"},
+    { ip: "192.168.13.185", desc: 'Certificados', status: "ONLINE"},
+    { ip: "192.168.13.155", desc: 'Faro scene', status: "ONLINE"},
+    { ip: "192.168.13.126", desc: 'Remota (Falla bios)', status: "ONLINE"},
+    { ip: "192.168.13.110", desc: 'Remota Procesos', status: "ONLINE"},
+    { ip: "192.168.13.113", desc: 'Remota', status: "ONLINE"},
+    { ip: "192.168.13.176", desc: 'Remota', status: "ONLINE"},
+    { ip: "192.168.13.100", desc: 'Remota', status: "ONLINE"},
+    { ip: "192.168.12.26", desc: 'Bejerman', status: "ONLINE"},
+    { ip: "192.168.13.121", desc: 'Remota', status: "ONLINE"},
+    { ip: "192.168.13.145", desc: 'Remota externos', status: "ONLINE"},
+  ]
 var frequency = 1000;
 //hacer de host un array y poner la ip en {0} y el nombre de la pc en {1} ubicacion en {2}
+// console.log(host2[0].ip)
 
-host2.forEach(function(host){ // aca seria host2.forEach(function(host{0}}
+
+host2.forEach(function(host){ 
     let count = 0 ;
+    // console.log(host.ip)
     let countConsoleOutput = 0;
     setInterval(function() {
-
-ping.sys.probe( host , function(isAlive) {
+ping.sys.probe( host.ip , function(isAlive) {
 
     // announce contact status
-    var info = (isAlive?host + " contact":host +" contact:lost")
+    var info = (isAlive?host.ip + " contact":host.ip +" contact:lost")
     
     if (!isAlive) {
+        // host.status = "OFFLINE"
+
         count++
         if (count === 3){
             if (countConsoleOutput < 1){
-                let consoleOutput = console.log("\x1b[5m", "\x1b[37m", "\x1b[41m", info, "\x1b[0m");
-                consoleOutput;
+                // host.status ="\x1b[5m", "\x1b[37m", "\x1b[41m", info, "\x1b[0m";
+                host.status = ('\x1b[91m \x1b[37m \x1b[41m OFFLINE \x1b[0m');
                 countConsoleOutput = 2;
-
             
             }
         }else if(count > 3 ){
@@ -31,10 +50,28 @@ ping.sys.probe( host , function(isAlive) {
     if(isAlive){
 
         if (countConsoleOutput > 1){
-        console.log(info + " esta de vueltas online")
-        countConsoleOutput = 0;
+            host.status = ('\x1b[32m ONLINE \x1b[0m');
+            countConsoleOutput = 0;
         }
     }
     });
 }, frequency);
+
 });
+setInterval(reloadTable, 10000);
+function reloadTable(){
+    console.clear();
+console.log(Table.print(host2))
+}
+
+  
+//   data.forEach(function(remota) {
+//     t.cell('Product Id', remota.ip)
+//     t.cell('Description', remota.desc)
+//     t.cell('Price, USD', remota.status, Table.number(2))
+//     t.newRow()
+//   })
+  
+//   console.log(Table.print(data))
+
+//   console.log(t.toString())
